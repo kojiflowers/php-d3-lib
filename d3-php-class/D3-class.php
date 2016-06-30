01a01ee91bbf2d3c2cd0a494f5771fcd35e77893
Koji Flowers <kojiflowers@gmail.com>
2016-06-30 13:02:53 -0400

    init commit - general file setup - ideas for chart types

---
+++ d3-php-class/D3-class.php
@@ -0,0 +1,29 @@
+<?php
+
+// ref: https://github.com/d3/d3/wiki/Gallery
+
+class D3{
+
+    function __construct()
+    {
+
+    }
+
+    /**
+     * Build js
+     */
+    function build(){
+
+    }
+
+    function simple_pie_chart(){
+
+    }
+
+    // https://bl.ocks.org/mbostock/3885304
+    function simple_bar_chart(){
+
+    }
+
+
+}
